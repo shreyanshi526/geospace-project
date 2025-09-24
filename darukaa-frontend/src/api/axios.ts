e229cac6d2dff538e22f4501383ea2c587dae004
Shreyanshi.kushwaha <apple@ZEVO-LAP-032.local>
2025-09-24 16:49:31 +0530

    feat(frontend): updated code to use app url from env

--- darukaa-frontend/src/api/axios.ts
+++ darukaa-frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // update baseURL
+  baseURL: process.env.REACT_APP_API_URL, // update baseURL
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
